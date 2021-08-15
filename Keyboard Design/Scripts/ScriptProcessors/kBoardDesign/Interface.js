a84ea2eda695ff9b653e4e3cefa297229627cecd
Mathieu <84969276+Matt-Dub@users.noreply.github.com>
2021-08-15 13:44:21 +0200

    Add files via upload

---
+++ Keyboard Design/Scripts/ScriptProcessors/kBoardDesign/Interface.js
@@ -0,0 +1,22 @@
+Content.makeFrontInterface(500, 200);
+function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
